fe00967f3699ca45402f7269c0a82e08ff87faa9
Parth Paradkar <parthparadkar3@gmail.com>
2019-07-20 16:50:19 +0530

    Creation of DB Model methods (#25)
    * uuid added to dependencies
    
    * Basic user model methods created
    
    * Updated
    
    * id code deleted
    
    * save_changes function added
    
    * docstrings updated
    
    * uuid removed from dependencies

--- app/main/service/user_service.py
+++ app/main/service/user_service.py
@@ -1,1 +1,38 @@
 # for user related operations
+
+import datetime
+
+from app.main import db
+from app.main.models.user import User
+
+def save_new_user(data):
+    """ Creates a new user """
+    user = User.query.filter_by(email=data['email']).first()
+    if not user:
+        new_user = User(
+            email=data['email'],
+            username=data['username'],
+            password=data['password'],
+            registered_on=datetime.datetime.utcnow()
+        )
+        save_changes(new_user)
+        response = {
+            'status': 'success',
+            'message': 'User successfully created'
+        }
+        return response, 201
+    else:
+        response = {
+            'status': 'fail',
+            'message': 'User already exists'
+        }
+        return response, 409
+
+def get_all_users():
+    """ Gets all users """
+    return User.query.all()
+
+def save_changes(data):
+    """ Saves changes to the database """
+    db.session.add(data)
+    db.session.commit()

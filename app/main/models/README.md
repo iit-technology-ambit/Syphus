c9c8460b9a7bd099c9bc0477799fa7e5a68c0f8b
Mukul Mehta <mukul.csiitkgp@gmail.com>
2019-07-20 15:48:57 +0530

    Added Users and Messages Model and updated README.md

--- app/main/models/README.md
+++ app/main/models/README.md
@@ -1,7 +1,7 @@
 ### MODELS
 
 This directory gives all the DB models using SQLAlchemy's db.Model class. 
-The Database Models are listed on the following link:   https://dbdiagram.io/d/5d1bb205ced98361d6dc458c  
+Current DB Diagram in use: https://dbdiagram.io/d/5d1cdccfced98361d6dc4fde
 
 The structure is given as follows:
 ```` 
@@ -17,3 +17,14 @@ The structure is given as follows:
 
 # Creating A New Model
 Update this file to reflect directory structure correctly and describe the contents of the created DB Table.
+## Keep in mind the following things while creating new models.
+
+* Create one Model class in one file only.  
+* Name of class will start from Capital letter and follow Camel Case.  
+* Class Name should be Singular whereas the file name will be the plural form of the class name
+* If you use a custom Error Class, define it in errors.py and import it, same goes for enums.  
+* If your model has many-to-many relationship with any other model, you need to define a junction table. For that, check the dbdiagram. Add the table in the file that contains the first class in the junction table's name.  
+* If you are unsure that one db.relationship is defined in your model's target model or not, define the same in your model. Redundancy must be removed later.  
+## Testing
+Always test the model locally and rollback changes in your local db after all tests are complete.  In no case, use the production database for testing. Also do not hard-code username, password or  db urls. Use environment variables for the same.  
+
---
+++ app/main/models/users.py
@@ -0,0 +1,70 @@
+"""
+DB Model for Users table and
+Junction Table relating
+Users and Tags
+"""
+
+from . import db
+from enums import PriorityType
+import datetime
+
+class User(db.Model):
+	"""
+	Description of User model.
+	Columns
+	-----------
+	:id: int [pk]	
+	:username: varchar(128) [not NULL]
+	:password: varchar(128) [not NULL]
+	:first_name: varchar(255) [not NULL]
+	:last_name: varchar(255)
+	:dob: date
+	:email: varchar(255) [not NULL]
+	:fb_handle: varchar(255)
+	:g_handle: varchar(255)
+	:medium_handle: varchar(255)
+	:twitter_handle: varchar(255)
+	:linkedin_handle: varchar(255)
+	:bio: text
+	:occupation: varchar(255)
+	:profile_picture: int
+	:last_login: timestamp
+	:creation_time: timestamp
+	"""
+
+	#Columns
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(128), nullable=False)
+	password = db.Column(db.String(128), nullable=False)
+	first_name = db.Column(db.String(255), nullable=False)
+	last_name = db.Column(db.String(255), nullable=False)
+	dob = db.Column(db.DateTime)
+	email = db.Column(db.String(255), nullable=False)
+	fb_handle = db.Column(db.String(255))
+	g_handle = db.Column(db.String(255))
+	medium_handle = db.Column(db.String(255))
+	twitter_handle = db.Column(db.String(255))
+	linkedin_handle = db.Column(db.String(255))
+	profile_picture = db.Column(db.Integer)
+	bio = db.Column(db.Text)
+	occupation = db.Column(db.String(255))
+	last_login = db.Column(db.DateTime)
+	creation_time = db.Column(db.DateTime)
+
+	#Relationships
+
+	
+
+
+	#Junction Table relating Users and Tags
+	userTagJunction = db.Table('userTagJunction' ,
+								db.Column('user_id', db.Integer,
+										  db.ForeignKey(user.id),
+										  primary_key = True) ,
+								db.Column('keyword_id', db.Integer,
+										   db.ForeignKey(tag.id) ,
+										   primary_key = True) ,
+								db.Column('priority' , db.Enum(PriorityType))
+
+
+   )

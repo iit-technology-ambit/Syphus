36cc870875cb5ad3ba527cb3338ed6dab4bca663
Mukul Mehta <mukul.csiitkgp@gmail.com>
2019-07-21 16:37:24 +0530

    Added Sendgrid Utility to send mails (#30)
    * Added Sendgrid Utility to send mails
    
    * Updated sendgrid file
    
    Added proposed Model methods (#31)
    
    * Added models for posts and tags. Added separate file for enums.
    Added flask_session, flask_bcrypt to dependencies.
    
    * New model added:
    imgLinks
    Modified: posts
    Added: errors.py for all Custom Error Classes
    
    * Added README.md
    
    * Update README.md
    
    * Update README.md
    
    * Removed Merge Conflicts
    
    * Removed merge conflicts
    
    * Changed property to hybrid_property in Post model
    
    * DB Model Updates
    
    * Cleansed PR
    
    * Added model methods
    
    * Added DB Model methods
    
    * Added verified email column
    
    * Changed the method of accessing Config
    
    * Added default value for is_verified column
    
    * Changes in users.py for flask-login
    
    Delete and add tag endpoints added

--- app/main/controller/tag_controller.py
+++ app/main/controller/tag_controller.py
@@ -1,5 +1,6 @@
 # endpoint for tag related operations
 from flask_restplus import Resource
+from flask import request, abort
 from app.main import db
 from app.main.models.tags import Tag
 
@@ -9,13 +10,29 @@ from ..util.tagDto import TagDto
 api = TagDto.api
 _tag = TagDto.tag
 
-# Routes
 
 @api.route('/tag/getAll')
 class AllTags(Resource):
     @api.doc('list of all tags')
     @api.marshal_list_with(_tag)
     def get(self):
-        """Gets all tags"""
-        # Make db call
         return Tag.query.all()
+
+@api.route('/tag/remove/<int:id>')
+class DeleteTag(Resource):
+    def delete(self, id):
+        tag = Tag.getTag(id)
+        if tag is not None:
+            tag.delete()
+            return "Tag deleted", 201
+        else:
+            abort(404)
+
+@api.route('/tag/add')
+class AddTags(Resource):
+    @api.doc(params={ 'tags': 'List of tags to be added' })
+    def post(self):
+        tag_list = request.form['tags']
+        for tag in tag_list:
+            new_tag = Tag(tag)
+        return "tags added", 201
--- app/main/models/imgLinks.py
+++ app/main/models/imgLinks.py
@@ -1,5 +1,10 @@
 """DB Model for imgLink table"""
 from . import db
+from flask import current_app
+import datetime
+from logging import getLogger
+import os
+LOG = getLogger(__name__)
 
 class ImgLink(db.Model):
     """
@@ -11,6 +16,54 @@ class ImgLink(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     link = db.Column(db.String(256), nullable=False)
+
+    def __init__(self, imageBinary):
+        """
+        Pass a binary image in imageBinary and it will be saved.
+        Image Naming Convention: present utc timestamp.png
+        [Needs to be changed]
+        """
+        fname = datetime.datetime.now() + ".png"
+        file = open(os.path.join(current_app.config["IMGDIR"], fname), "wb")
+        LOG.info("Writing new image to disk, %s", fname)
+        file.write(imageBinary)
+        file.close()
+
+        self.link = os.path.join(current_app.config["IMGDIR"], fname)
+
+        LOG.info("New imgLink added to database.")
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        LOG.info("Removing image from disk")
+        os.remove(self.link)
+
+        LOG.info("ImgLink deleted: %s", self.link)
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, newImgBin):
+        id = self.id
+        self.delete()
+
+        self.id = id
+        fname = datetime.datetime.now() + ".png"
+        file = open(os.path.join(current_app.config["IMGDIR"], fname), "wb")
+        LOG.info("Writing new image to disk, %s", fname)
+        file.write(newImgBin)
+        file.close()
+
+        self.link = os.path.join(current_app.config["IMGDIR"], fname)
+
+        LOG.info("New imgLink added to database.")
+        db.session.add(self)
+        db.session.commit()
+
+
+
+
+
 
 imgPostJunction = db.Table('imgPostJunction',
                             db.Column('img_id', db.Integer,

ac321180bd2d30cf40bcdf0d21d5705d1d60a871
Shubham Mishra <donofsuri@gmail.com>
2019-07-24 17:58:00 +0530

    Article endpoints

--- app/main/controller/article_controller.py
+++ app/main/controller/article_controller.py
@@ -4,6 +4,7 @@ from app.main.util.dto import PostDto
 from app.main.models.posts import Post
 from app.main.models.users import User
 from app.main.models.imgLinks import ImgLink
+from app.main.models.errors import LoginError
 from logging import getLogger
 
 LOG = getLogger(__name__)
@@ -12,11 +13,11 @@ bp = Blueprint("article", __name__, url_prefix='/article')
 api = Api(bp)
 api.add_namespace(PostDto.api)
 
-@api.route("/<int:id>")
+@api.route("/<int:post_id>")
 class ArticleFetch(Resource):
     @api.marshal_with(PostDto.article)
-    @api.doc(params={'id': 'Post Id'})
-    def get(self, id):
+    @api.doc(params={'post_id': 'Post Id'})
+    def get(self, post_id):
         """
         Fetches the article given by the id.
         """
@@ -47,7 +48,25 @@ class ArticleCreator(Resource):
         LOG.info("New Post Created")
         return "Post Created", 201
 
-            
+@api.route("/save/<int:post_id>")
+class ArticleSave(Resource):
+    @api.doc(params={"id": "post_id"})
+    def post(self, post_id):
+        post = Post.getArticles(id)
+        #First define what data is to be sent for user validation
+        raise LoginError
+
+@api.route("/rate/<int:post_id>")
+class ArticleRate(Resource):
+    @api.doc(params={"id": "post_id"})
+    def post(self, post_id):
+        raise LoginError
+
+@api.route("/by_tags")
+class ArticleByTag(Resource):
+    @api.doc(
+
+
 
 
 

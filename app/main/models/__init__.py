dbe449fcaae62d24b478fb39c32c1ec877709472
Shivam Kumar Jha <shivam.cs.iit.kgp@gmail.com>
2019-07-30 21:20:10 +0530

    Update __init__.py

--- app/main/models/__init__.py
+++ app/main/models/__init__.py
@@ -1,8 +1,1 @@
-from app.main import db
-from app.main.models.enums import *
-from app.main.models.errors import *
-from app.main.models.imgLinks import *
-from app.main.models.payments import *
-from app.main.models.posts import *
-from app.main.models.tags import *
-from app.main.models.users import *
+

f4ea3d05ab82786ba7a74ef55af6d97cade924b0
Mukul Mehta <mukul.csiitkgp@gmail.com>
2019-08-15 01:16:53 +0530

    Added Flask-CORS
    Added Flask-CORS

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from app.main.config import config_by_name
 
@@ -20,14 +21,17 @@ def create_app(config_name):
     app = Flask(__name__, template_folder="../static/templates")
     app.config.from_object(config_by_name[config_name])
     LOG.info('app loaded with configuration!')
-    
+
     db.init_app(app)
     LOG.info('database initialized successfully!')
-    
+
     flask_bcrypt.init_app(app)
     LOG.info('flask encryption initialized successfully!')
 
     login_manager.init_app(app)
     LOG.info('Flask-Login set up successfully!')
+
+    CORS(app)
+    LOG.info("Flask-CORS set up succesfully!")
 
     return app
